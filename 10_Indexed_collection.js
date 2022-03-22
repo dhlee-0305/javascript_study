c7d51cc8682e0020b509258ad112ce575b2c18c8
이대현 <dhlee@gaeasoft.co.kr>
2022-03-22 18:17:40 +0900

    배열 객체 1차 커밋

---
+++ 10_Indexed_collection.js
@@ -0,0 +1,11 @@
+
+//https://developer.mozilla.org/ko/docs/Web/JavaScript/Guide/Indexed_collections
+
+
+let myList = [, 'home', 'school'];
+console.log(myList.length);
+
+let wisenArray = Array.of(9.3);
+console.log(wisenArray);
+
+//배열에 값 저장 : https://developer.mozilla.org/ko/docs/Web/JavaScript/Guide/Indexed_collections#%EB%B0%B0%EC%97%B4%EC%97%90_%EA%B0%92_%EC%A0%80%EC%9E%A5
